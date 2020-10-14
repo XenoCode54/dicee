955f103c1098446a3b8be0417403e765bd2b2c53
{xenon} <{macolawale@gmail.com}>
2020-10-14 23:06:25 +0100

    Update gradle-wrapper.properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,18 +8,57 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(
+            child: Text('Dicee'),
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
       ),
     ),
   );
+} // Main
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
 }
 
-class DicePage extends StatelessWidget {
+class _DicePageState extends State<DicePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  ice();
+                });
+              },
+              child: Image.asset('images/dice$leftDiceNo.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  ice();
+                });
+              },
+              child: Image.asset('images/dice$rightDiceNo.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
+}
+
+int leftDiceNo = 5;
+int rightDiceNo = 1;
+void ice() {
+  leftDiceNo = Random().nextInt(6) + 1;
+  rightDiceNo = Random().nextInt(6) + 1;
 }
